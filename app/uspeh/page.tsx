26bc162d63e85d846aa02019040fba55982b1d71
matickirbis-creator <matic.kirbis@gmail.com>
2025-08-27 23:40:45 +0200

    Add files via upload

--- app/uspeh/page.tsx
+++ app/uspeh/page.tsx
@@ -1,23 +1,34 @@
 'use client';
+import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 import '../globals.css';
 import Link from 'next/link';
 
-export default function UspehPage() {
+function UspehContent() {
   const params = useSearchParams();
   const toPatient = params.get('patient') === '1';
   return (
+    <>
+      <h1>Uspeh 🎉</h1>
+      <p style={{ marginTop: 8, fontSize: 18 }}>
+        {toPatient
+          ? 'Vaš obrazec je podpisan in poslan na vaš elektronski naslov.'
+          : 'Vaš obrazec je podpisan in poslan v ordinacijo.'}
+      </p>
+      <div style={{ marginTop: 24 }}>
+        <Link href="/" className="button">Izpolni nov obrazec</Link>
+      </div>
+    </>
+  );
+}
+
+export default function UspehPage() {
+  return (
     <div className="container">
       <div className="card" style={{ textAlign: 'center' }}>
-        <h1>Uspeh 🎉</h1>
-        <p style={{ marginTop: 8, fontSize: 18 }}>
-          {toPatient
-            ? 'Vaš obrazec je podpisan in poslan na vaš elektronski naslov.'
-            : 'Vaš obrazec je podpisan in poslan v ordinacijo.'}
-        </p>
-        <div style={{ marginTop: 24 }}>
-          <Link href="/" className="button">Izpolni nov obrazec</Link>
-        </div>
+        <Suspense fallback={<p>Nalaganje ...</p>}>
+          <UspehContent />
+        </Suspense>
       </div>
     </div>
   );
